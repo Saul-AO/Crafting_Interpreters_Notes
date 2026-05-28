d7b95013f8bffe55f15a6a0c6256c81291c0ade5
Saul Aguilar <Aguilar-Saul@proton.me>
2026-05-28 12:55:39 -0500

    Starting implementation of jlox interpreter

---
+++ JLOX/Lox.java
@@ -0,0 +1,63 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+  static boolean hadError = false;
+  // * This is a checker for the arguments inputted. For it to run
+  // * successfully, you need either one or none.
+  // * Example: 'jlox script.lox' or 'jlox' will work
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
+      System.exit(64); 
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+  }
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+
+    // Indicate an error in the exit code.
+    if (hadError) System.exit(65);
+  }
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+    // * This is an infinite loop which allows the user to interact
+    // * with the program
+    for (;;) {
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null) break;
+      run(line);
+      hadError = false;
+    }
+  }
+  private static void run(String source) {
+    Scanner scanner - new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+    for (Token token : tokens) {
+      System.out.println(token);
+    }
+  }
+  // ! Error handling
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  private static void report(int line, String where, String message) {
+    System.err.println("[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
+  }
+
+}
